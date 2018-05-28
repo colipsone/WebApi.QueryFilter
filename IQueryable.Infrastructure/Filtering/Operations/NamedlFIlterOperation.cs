bf3761b66683239857b183d5320d89fb31439a16
mkriukov <mkriukov@vantagefreight.com>
2018-05-28 23:48:49 +0300

    Named operation now moved to separate filter file too

---
+++ IQueryable.Infrastructure/Filtering/Operations/NamedlFIlterOperation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace IQueryableFilter.Infrastructure.Filtering.Operations
+{
+    internal class NamedlFilterOperation : IFilterOperation
+    {
+        public string OperationShorthand => "named";
+
+        public FilterOperationType Type => FilterOperationType.Named;
+
+        public Expression GetOperationExpression(Expression left, Expression right)
+        {
+            throw new NotSupportedException("Named operations is not supported in this context!");
+        }
+    }
+}
